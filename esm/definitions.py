0db3f2f2635a1ce2d3b92cd48c5c64eac467f103
sturdy-robot <pedrenriquegg@hotmail.com>
2024-04-14 15:06:23 -0300

    fix: remove unused import

--- esm/definitions.py
+++ esm/definitions.py
@@ -13,8 +13,6 @@
 #
 #      You should have received a copy of the GNU General Public License
 #      along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-import os
 from pathlib import Path
 
 ROOT_DIR = Path(__file__).parent
