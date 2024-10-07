7052768e74456651c2c7d89cd8909d9d06860c29
sturdy-robot <pedrenriquegg@hotmail.com>
2024-10-07 17:43:34 -0300

    fix: make darkMode toggle work with tailwind

--- esm_ui/tailwind.config.js
+++ esm_ui/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./templates/**/*.{html,htm,js}"],
+    darkMode: "selector",
     theme: {
         extend: {},
     },
